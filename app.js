3d669ee1a60b5cc5c176e5023f2811a89ac0dd4e
Fernando <fernandowashimi@gmail.com>
2020-05-26 23:27:36 -0300

    Change Procfile

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import * as flags from "https://deno.land/std@v0.50.0/flags/mod.ts";
+import { config } from "https://deno.land/x/dotenv/mod.ts"
 // import router from "./routes.js";
 
 const DEFAULT_PORT = 8080;
 const argPort = flags.parse(Deno.args).port;
 const port = argPort ? Number(argPort) : DEFAULT_PORT;
 
+console.log(config())
 console.log(flags.parse(Deno.args))
 
 class App {
